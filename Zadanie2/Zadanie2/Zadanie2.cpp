5d681f031bf1bd2ebc5f2be837381bbbef40ed34
Danil <DanielDraw98@github.com>
2019-09-04 15:06:11 +0500

    pervoe

---
+++ Zadanie2/Zadanie2/Zadanie2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+	int a[10];
+	int c1 = 0, c2 = 0;
+	for (int i = 0; i < 10; i++) {
+		cin >> a[i];
+	}
+	for (int i = 0; i < 10; i++) {
+		if (a[i] % 2 == 0) {
+			c1++;
+		}
+		else {
+			c2++;
+		}
+	}
+	cout << c1 << endl;
+	cout << c2 << endl;
+}
